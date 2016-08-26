44a1a3ef7ef5aea8d11b8182d8ff11c3153f94b8
初谷怜慈 <reiji@hatsutanireiji-no-MacBook-Pro.local>
2016-08-26 16:30:04 +0900

    work

--- analogy_m1_m2.py
+++ analogy_m1_m2.py
@@ -1,11 +1,11 @@
 from keras.models import load_model
 from keras.datasets import mnist
 from keras.utils import np_utils
+import numpy as np
 import matplotlib.pyplot as plt
 from custom_batchnormalization import CustomBatchNormalization
 
-custom_objects={'CustomBatchNormalization': CustomBatchNormalization}
-
+custom_objects = {'CustomBatchNormalization': CustomBatchNormalization}
 
 if __name__ == '__main__':
     (X_train, y_train), (X_test, y_test) = mnist.load_data()
@@ -17,34 +17,29 @@ if __name__ == '__main__':
     X_train[X_train <= 0.5] = 0.0
     X_test[X_test > 0.5] = 1.0
     X_test[X_test <= 0.5] = 0.0
-    y_train = np_utils.to_categorical(y_train)
-    y_test = np_utils.to_categorical(y_test)
+    y_train = np_utils.to_categorical(y_train, 10)
+    y_test = np_utils.to_categorical(y_test, 10)
 
     encoder_m1 = load_model('./trained_model/encoder_m1.h5', custom_objects=custom_objects)
     decoder_m1 = load_model('./trained_model/decoder_m1.h5', custom_objects=custom_objects)
     encoder_m2 = load_model('./trained_model/encoder_m2.h5', custom_objects=custom_objects)
     decoder_m2 = load_model('./trained_model/decoder_m2.h5', custom_objects=custom_objects)
 
-    targets = X_train[0:5]
-    labels = y_train[0:5]
-
-
-    z1 = encoder_m1.predict(targets, batch_size=5)
-    z2 = encoder_m2.predict([z1, labels], batch_size=5)
-    reconstruct_z1 = decoder_m2.predict([labels, z2], batch_size=5)
-    reconstruct_images = decoder_m1.predict(reconstruct_z1, batch_size=5)
+    X_targets = X_train[9:17]
+    y_targets = y_train[9:17]
+    z1 = encoder_m1.predict(X_targets, batch_size=8)
+    z2 = encoder_m2.predict([z1, y_targets], batch_size=8)
 
     fig = plt.figure(figsize=(14, 14))
-    for i, target in enumerate(targets):
-        ax = fig.add_subplot(2, 5, i+1, xticks=[], yticks=[])
-        ax.imshow(target.reshape(28, 28), 'gray')
-    for i, reconstruct_image in enumerate(reconstruct_images):
-        ax = fig.add_subplot(2, 5, 6+i, xticks=[], yticks=[])
-        ax.imshow(reconstruct_image.reshape(28, 28), 'gray')
-    plt.savefig('./images/reconstruct_m1_m2.png')
-
-
-
+    for i, z in enumerate(z2):
+        ax = fig.add_subplot(8, 11, 11*i+1, xticks=[], yticks=[])
+        ax.imshow(X_targets[i].reshape(28, 28), 'gray')
+        for j, y in enumerate(np.eye(10)):
+            z1_reconstruct = decoder_m2.predict([y.reshape(1, -1), z2[i].reshape(1, -1)], batch_size=1)
+            x_reconstruct = decoder_m1.predict(z1_reconstruct, batch_size=1)
+            ax = fig.add_subplot(8, 11, 11*i+j+2, xticks=[], yticks=[])
+            ax.imshow(x_reconstruct.reshape(28, 28), 'gray')
+    plt.savefig('./images/analogy_m1_m2.png')
 
 
 
